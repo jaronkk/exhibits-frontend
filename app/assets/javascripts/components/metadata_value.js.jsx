09a63a4b0039a9b76fb154e0afd02ac704406b79
Jaron Kennel <jaronkk@gmail.com>
2015-02-11 14:53:46 -0800

    Automatically link urls in metadata values

--- app/assets/javascripts/components/metadata_value.js.jsx
+++ app/assets/javascripts/components/metadata_value.js.jsx
@@ -3,10 +3,31 @@ var MetadataValue = React.createClass({
     value: React.PropTypes.any.isRequired,
   },
 
+  // Pattern copied from https://github.com/bryanwoods/autolink-js
+  autoLink: function() {
+    var value = this.props.value;
+    var pattern = /(^|[\s\n]|<br\/?>)((?:https?|ftp):\/\/[\-A-Z0-9+\u0026\u2019@#\/%?=()~_|!:,.;]*[\-A-Z0-9+\u0026@#\/%=~()_|])/gi;
+    if (pattern.test(value)) {
+      var matches = value.split(pattern);
+      var replacedNodes = matches.map(function(string, index) {
+        if (pattern.test(string)) {
+          return (
+            <a href={string} key={index} target="_blank" rel="nofollow">{string}</a>
+          );
+        } else {
+          return string;
+        }
+      });
+      return replacedNodes;
+    } else {
+      return value;
+    }
+  },
+
   render: function() {
     return (
       <dd className="metadata-value">
-        {this.props.value}
+        {this.autoLink()}
       </dd>
     );
   }
