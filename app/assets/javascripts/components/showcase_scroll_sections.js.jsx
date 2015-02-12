71c3350ff1dd3bb9cdadf188eb3318c0f5900a4a
Jaron Kennel <jaronkk@gmail.com>
2015-02-12 10:58:54 -0800

    Removed extra section div around columns

--- app/assets/javascripts/components/showcase_scroll_sections.js.jsx
+++ app/assets/javascripts/components/showcase_scroll_sections.js.jsx
@@ -19,9 +19,22 @@ var ShowcaseScrollSections = React.createClass({
 
   render: function() {
     var sectionNodes = this.state.sections.map(function(section) {
-      return (
-        <ShowcaseScrollSection key={section['@id']} section={section} />
-      )
+      var columns = [];
+      if (section['hasPart/item']) {
+        columns.push((
+          <div key={'item' + section['@id']} className="showcase-scroll-section-column">
+            <ShowcaseScrollSectionItem itemUrl={section['hasPart/item']} />
+          </div>
+        ));
+      }
+      if (section.description) {
+        columns.push((
+          <div key={'text' + section['@id']} className="showcase-scroll-section-column">
+            <ShowcaseScrollSectionText name={section.name} description={section.description} />
+          </div>
+        ));
+      }
+      return columns;
     });
     return (
       <div className="showcase-scroll-sections">
