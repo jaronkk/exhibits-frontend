0f1141745132a693a14a6c332e6d389d37139605
Jaron Kennel <jaronkk@gmail.com>
2015-02-12 10:19:49 -0800

    More showcase styling

--- app/assets/javascripts/components/showcase_scroll_section_text.js.jsx
+++ app/assets/javascripts/components/showcase_scroll_section_text.js.jsx
@@ -7,8 +7,10 @@ var ShowcaseScrollSectionText = React.createClass({
   render: function() {
     return (
       <div className="showcase-scroll-section-text">
-        <h2>{this.props.name}</h2>
-        <div dangerouslySetInnerHTML={{__html: this.props.description}} />
+        <div className="showcase-scroll-section-text-frame">
+          <h2>{this.props.name}</h2>
+          <div dangerouslySetInnerHTML={{__html: this.props.description}} />
+        </div>
       </div>
     );
   }
